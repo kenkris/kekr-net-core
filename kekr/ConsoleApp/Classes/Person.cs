c592bbbc4f4b9036b196af0a8fffa46cde1a8755
Kenneth Kristensen <kekr@SCANMARKET.local>
2018-08-13 12:54:11 +0200

    Add base console app project

---
+++ kekr/ConsoleApp/Classes/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Classes
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        private const string TextHello = "Hello";
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public string SayHello()
+        {
+            return TextHello;
+        }
+        public string SayHello(Person person)
+        {
+            return TextHello + $" {person.FirstName} {person.LastName}";
+        }
+    }
+}
---
+++ kekr/ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp.Classes;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person1 = new Person("Mogen", "Munjasa");
+            var person2 = new Person("Abe", "Lorentzen");
+
+            Console.WriteLine(person1.SayHello());
+            Console.WriteLine(person1.SayHello(person2));
+
+            Console.ReadLine();
+        }
+    }
+}
